e7854e9792501083c96908ea0221166e718f3ca9
brendena <cerebralasylum1@gmail.com>
2017-03-14 10:37:13 -0400

    i added comments and played with the css

---
+++ README.md
@@ -0,0 +1,11 @@
+# sound-rebound-all-together
+
+sound-rebound-all-together is a combination of lots of smaller projects put together.
+
+[sound-rebound-NN-creation](https://github.com/brendena/sound-rebound-NN-creation)
+
+[sound-rebound-matrix-creator](https://github.com/brendena/sound-rebound-matrix-creator)
+
+[sound-rebound-other-stuff](https://github.com/brendena/sound-rebound-other-stuff)
+
+Each part had a destinct function that i need to flesh out before intergrating all of them together.
--- sound-rebound-matrix-creator/raspberryPiInterface/index.py
+++ sound-rebound-matrix-creator/raspberryPiInterface/index.py
@@ -9,40 +9,73 @@ app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
 
 
+port = 5035
 
 
-
-
-
-
+'''!!!!!!#!!!!!!!!!!!!!!!!!#!!!!!!!!!!!!#!!!!
+/$$$$$$$$$$$$$____bad_code____$$$$$$$$$$$$$$$
+/!!!!!!#!!!!!!!!!!!!!!!!!#!!!!!!!!!!!!#!!!!!!
+/
+/   The below code is for updating the
+/   account object.  I have to attach
+/   a function to the firebase stream
+/   so when it changes it fire and
+/   sends the data to all the clients
+/
+/   This is what the callBackFunction
+/   is for.  It get pushed down to the
+/   base firebase.py object.  
+/
+/   `Order Matters`
+/   I need to first create myFlaskFirebase
+/   because updateUsersData uses myFlaskFirebase.
+/   So i have to first create myFlaskFirebase,
+/   and pase the callBackFunction with the init
+/   method.
+/
+!!!!!!#!!!!!!!!!!!!!!!!!#!!!!!!!!!!!!#!!!!'''
 myFlaskFirebase = flaskFirebase()
-
-port = 5028
 
 def updateUsersData():
     accounts = myFlaskFirebase.getFlasksAccountInfo()
+    ''' reason for with app.test
+    http://stackoverflow.com/questions/31647081/flask-python-and-socket-io-multithreading-app-is-giving-me-runtimeerror-work'''
     with app.test_request_context('/'):
         emit("allAccounts",{"data":accounts}, broadcast=True,namespace='/')
 
-def coolFunction():
+def callBackFunction():
     updateUsersData()
 
-myFlaskFirebase.init(coolFunction)
+myFlaskFirebase.init(callBackFunction)
+
+'''!!!!!!#!!!!!!!!!!!!!!!!!#!!!!!!!!!!!!#!!!!
+/$$$$$$___end_of_bad_code____$$$$$$$$$$$$$$$
+!!!!!!#!!!!!!!!!!!!!!!!!#!!!!!!!!!!!!#!!!!'''
 
 
-
-
+'''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/ on `add_account` add account and the 
+/ information will be updated when it get the
+/ information from the stream.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
 @socketio.on('add_account')
 def add_account(emailJson):
     print(emailJson)
     error = myFlaskFirebase.addUser(account=emailJson["emailAddress"], password=emailJson["password"])
 
 
+'''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/ on `remove_account` remove account and
+/ resend all the data.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
 @socketio.on('remove_account')
 def remove_account(emailJson):
     myFlaskFirebase.removeUser(emailJson["emailAddress"])
     updateUsersData()
 
+'''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/ on `connection` send all the users data
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
 @socketio.on('connect')
 def test_connect():
     updateUsersData()
@@ -57,6 +90,6 @@ def index():
 
 
 if __name__ == "__main__":
-    socketio.run(app,port=port,debug=True)
+    socketio.run(app,port=port)#,debug=True ,host='192.168.1.108'
     print("Running")
     
